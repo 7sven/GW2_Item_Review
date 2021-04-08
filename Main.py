142ad63b75f152b5e541bfb3bd536a9bc8b09a1c
Sven <sven0321@freenet.de>
2021-04-08 19:51:07 +0200

    created a connection to a data base, still need to add the items to the data base

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import urllib.request, json
+import urllib.request, json, sqlite3
 
 url_item_ids = 'https://api.guildwars2.com//v2/commerce/prices'
 url_item_name = 'https://api.guildwars2.com/v2/items?ids='
@@ -38,11 +38,13 @@ def get_prices(ids_to_check):
         item_reduced = []
         for i in range(len(all_200_names)):
             if (all_200_names[i]['id'] == all_200_items[i]['id']):
-                profit = int((all_200_items[i]['sells']['unit_price'] - (all_200_items[i]['sells']['unit_price'] * 0.15)) - all_200_items[i]['buys']['unit_price'])
-                if all_200_items[i]['buys']['unit_price'] != 0 :
-                    roi = ((profit / all_200_items[i]['buys']['unit_price']) * 10000)//100
+                profit = int(
+                    (all_200_items[i]['sells']['unit_price'] - (all_200_items[i]['sells']['unit_price'] * 0.15)) -
+                    all_200_items[i]['buys']['unit_price'])
+                if all_200_items[i]['buys']['unit_price'] != 0:
+                    roi = int(((profit / all_200_items[i]['buys']['unit_price']) * 10000) // 100)
                 else:
-                    roi = 0
+                    roi = int(0)
                 item_reduced.append(
                     [all_200_items[i]['id'], all_200_names[i]['name'], all_200_items[i]['buys']['unit_price'],
                      all_200_items[i]['sells']['unit_price'], profit, roi])
@@ -51,9 +53,18 @@ def get_prices(ids_to_check):
     return items_with_prices
 
 
-getid = get_all_ids()
+if __name__ == '__main__':
+    connect = sqlite3.connect('profit_table.db')
+    cursor = connect.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY,
+        name TEXT,
+        buy INTEGER,
+        sell INTEGER,
+        profit INTEGER,
+        roi INTEGER)''')
 
-prices = get_prices(getid)
+    get_id = get_all_ids()
+    items = get_prices(get_id)
 
-for p in prices:
-    print(p)
+    #cursor.executemany('INSERT INTO items VALUES (?,?,?,?,?)*, items') need to look into how to do this
