1dfacd03f715784caf93486dba88374343ef02b2
Sven <sven0321@freenet.de>
2021-04-09 18:07:26 +0200

    finished db insert and update of values

--- Main.py
+++ Main.py
@@ -46,25 +46,32 @@ def get_prices(ids_to_check):
                 else:
                     roi = int(0)
                 item_reduced.append(
-                    [all_200_items[i]['id'], all_200_names[i]['name'], all_200_items[i]['buys']['unit_price'],
-                     all_200_items[i]['sells']['unit_price'], profit, roi])
+                    (all_200_items[i]['id'], all_200_names[i]['name'], all_200_items[i]['buys']['unit_price'],
+                     all_200_items[i]['sells']['unit_price'], profit, roi))
         items_with_prices += item_reduced
         iterator_all_ids += 200
     return items_with_prices
+
+def update_db():
+    get_id = get_all_ids()
+    items = get_prices(get_id)
+    cursor.executemany('INSERT OR REPLACE INTO items VALUES (?,?,?,?,?,?)', items)
+    connect.commit()
 
 
 if __name__ == '__main__':
     connect = sqlite3.connect('profit_table.db')
     cursor = connect.cursor()
     cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY,
+        '''CREATE TABLE IF NOT EXISTS items (
+        id INTEGER PRIMARY KEY,
         name TEXT,
         buy INTEGER,
         sell INTEGER,
         profit INTEGER,
         roi INTEGER)''')
 
-    get_id = get_all_ids()
-    items = get_prices(get_id)
+    for row in cursor.execute('SELECT * FROM items ORDER BY id'):
+        print(row)
 
-    #cursor.executemany('INSERT INTO items VALUES (?,?,?,?,?)*, items') need to look into how to do this
+    connect.close()
