0fd555a5c398cff88822dd60ddbdb132182b890a
Sven <sven0321@freenet.de>
2021-04-10 18:21:04 +0200

    created gui and a loading screen for the update function

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-import urllib.request, json, sqlite3
+import urllib.request, json, sqlite3, tkinter as tk
+from tkinter import ttk
 
 url_item_ids = 'https://api.guildwars2.com//v2/commerce/prices'
 url_item_name = 'https://api.guildwars2.com/v2/items?ids='
@@ -31,9 +32,18 @@ return: list of lists in which each list contains id, name, sell and buy price, 
 
 
 def get_prices(ids_to_check):
+    popup = tk.Toplevel()
+    tk.Label(popup, text="Updating data base").grid(row=0, column=0, padx='5', pady='5')
+    progress_var = tk.DoubleVar()
+    progress = ttk.Progressbar(popup, orient='horizontal', mode="determinate", maximum=30000, variable=progress_var)
+    progress.grid(row=1, column=0, pady='5', padx='5')
+    popup.group(window)
+    popup.pack_slaves()
     items_with_prices = []
     iterator_all_ids = 0
+    progress.start()
     while iterator_all_ids < len(ids_to_check):
+        popup.update()
         id = ",".join([str(elem) for elem in ids_to_check[iterator_all_ids:iterator_all_ids + 200]])
         response_prices = urllib.request.urlopen(url_item_ids + "?ids=" + id)
         response_item_info = urllib.request.urlopen(url_item_name + id)
@@ -54,6 +64,8 @@ def get_prices(ids_to_check):
                      all_200_items[i]['sells']['unit_price'], profit, roi))
         items_with_prices += item_reduced
         iterator_all_ids += 200
+        progress_var.set(iterator_all_ids)
+    progress.stop()
     return items_with_prices
 
 
@@ -67,6 +79,10 @@ def update_db():
     items = get_prices(get_id)
     cursor.executemany('INSERT OR REPLACE INTO items VALUES (?,?,?,?,?,?)', items)
     connect.commit()
+
+
+def get_search():
+    pass
 
 
 """
@@ -84,7 +100,41 @@ if __name__ == '__main__':
         profit INTEGER,
         roi INTEGER)''')
 
-    for row in cursor.execute('SELECT * FROM items ORDER BY id'):
-        print(row)
+    window = tk.Tk()
+    window.title("Guild Wars 2 Item Profit Review")
+    window.geometry("700x700")
+
+    min_label = tk.Label(text="minimum buying price")
+    max_label = tk.Label(text="maximum buying price")
+    minimum_gold_price = ttk.Entry(width=5)
+    minimum_silver_price = ttk.Entry(width=5)
+    minimum_copper_price = ttk.Entry(width=5)
+    maximum_gold_price = ttk.Entry(width=5)
+    maximum_silver_price = ttk.Entry(width=5)
+    maximum_copper_price = ttk.Entry(width=5)
+    update_button = tk.Button(text="update data base", width=20, command=update_db)
+    get_results = tk.Button(text='search', width=5, command=get_search)
+    sort_after = ttk.Combobox(window, width=50, values=[
+        "sort after highest return of investment",
+        "sort cheapest to most expensive",
+        "sort most expansive to cheapest",
+        "sort after highest profit"
+    ])
+    results = ttk.Treeview()
+
+    min_label.grid(column=0, row=2, padx='5', pady='5', sticky='w')
+    minimum_gold_price.grid(column=1, row=2, padx='5', pady='5', sticky='w')
+    minimum_silver_price.grid(column=2, row=2, padx='5', pady='5', sticky='w')
+    minimum_copper_price.grid(column=3, row=2, padx='5', pady='5', sticky='w')
+    max_label.grid(column=4, row=2, padx='5', pady='5', sticky='w')
+    maximum_gold_price.grid(column=5, row=2, padx='5', pady='5', sticky='w')
+    maximum_silver_price.grid(column=6, row=2, padx='5', pady='5', sticky='w')
+    maximum_copper_price.grid(column=7, row=2, padx='5', pady='5', sticky='w')
+    get_results.grid(column=7, row=3)
+    sort_after.grid(column=0, columnspan=8, row=3)
+    update_button.grid(column=0, row=1, padx='5', pady='5', sticky='w')
+    results.grid(column=0, row=4, columnspan=9, padx='5', pady='5', sticky='NESW')
+
+    window.mainloop()
 
     connect.close()
